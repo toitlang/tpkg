4c19b222e14a3cfa6463f3e68a888f35f6de326c
Jesper Lindstrøm Nielsen <jesper@toitware.com>
2021-12-20 12:52:17 +0100

    Refactored RunWrapper and ViperConfigStore so they can be reused (#5)

--- cmd/toitpkg/main.go
+++ cmd/toitpkg/main.go
@@ -22,24 +22,13 @@ import (
 	"html/template"
 	"log"
 	"os"
-	"path/filepath"
 
-	"github.com/hashicorp/go-version"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"github.com/toitlang/tpkg/commands"
 	"github.com/toitlang/tpkg/config"
-	"github.com/toitlang/tpkg/pkg/tpkg"
+	"github.com/toitlang/tpkg/config/store"
 	"github.com/toitlang/tpkg/pkg/tracking"
 )
-
-type withExitCode interface {
-	ExitCode() int
-}
-
-type withSilent interface {
-	Silent() bool
-}
 
 var (
 	// Used for flag.
@@ -58,7 +47,6 @@ var (
 )
 
 func main() {
-	cobra.OnInitialize(initConfig)
 	// We use the configurations in the viperConf below.
 	// If we didn't want to use the globals we could also switch to
 	// a PreRun function.
@@ -75,23 +63,6 @@ func main() {
 
 	rootCmd.PersistentFlags().StringVar(&sdkVersion, "sdk-version", "", "The SDK version")
 
-	runWrapper := func(f commands.CobraErrorCommand) commands.CobraCommand {
-		return func(cmd *cobra.Command, args []string) {
-			err := f(cmd, args)
-			if err != nil {
-				_, ok := err.(withSilent)
-				if !ok {
-					fmt.Fprintf(os.Stderr, "Unhandled error: %v\n", err)
-				}
-				e, ok := err.(withExitCode)
-				if ok {
-					os.Exit(e.ExitCode())
-				}
-				os.Exit(1)
-			}
-		}
-	}
-
 	track := func(ctx context.Context, te *tracking.Event) error {
 		if shouldPrintTracking {
 			tmpl := template.Must(template.New("tracking").Parse(`Name: {{.Name}}
@@ -107,89 +78,21 @@ func main() {
 		return nil
 	}
 
-	pkgCmd, err := commands.Pkg(runWrapper, track, &viperConfigStore{}, nil)
+	configStore := store.NewViper(cacheDir, sdkVersion, noAutosync, noDefaultRegistry)
+	cobra.OnInitialize(func() {
+		if cfgFile == "" {
+			cfgFile, _ = config.UserConfigFile()
+		}
+		configStore.Init(cfgFile)
+	})
+
+	pkgCmd, err := commands.Pkg(commands.DefaultRunWrapper, track, configStore, nil)
 	if err != nil {
-		e, ok := err.(withSilent)
+		e, ok := err.(commands.WithSilent)
 		if !ok {
 			fmt.Fprintln(os.Stderr, e)
 		}
 	}
 	rootCmd.AddCommand(pkgCmd)
 	rootCmd.Execute()
-}
-
-func initConfig() {
-	if cfgFile == "" {
-		cfgFile, _ = config.UserConfigFile()
-	}
-	viper.SetConfigFile(cfgFile)
-	viper.ReadInConfig()
-}
-
-type viperConfigStore struct{}
-
-const packageInstallPathConfigEnv = "TOIT_PACKAGE_INSTALL_PATH"
-const configKeyRegistries = "pkg.registries"
-const configKeyAutosync = "pkg.autosync"
-
-func (vc *viperConfigStore) Load(ctx context.Context) (*commands.Config, error) {
-	result := commands.Config{}
-
-	if cacheDir == "" {
-		var err error
-		result.PackageCachePaths, err = config.PackageCachePaths()
-		if err != nil {
-			return nil, err
-		}
-		result.RegistryCachePaths, err = config.RegistryCachePaths()
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		result.PackageCachePaths = []string{filepath.Join(cacheDir, "tpkg")}
-		result.RegistryCachePaths = []string{filepath.Join(cacheDir, "tpkg-registries")}
-	}
-	if p, ok := os.LookupEnv(packageInstallPathConfigEnv); ok {
-		result.PackageInstallPath = &p
-	}
-	if sdkVersion != "" {
-		v, err := version.NewVersion(sdkVersion)
-		if err != nil {
-			return nil, err
-		}
-		result.SDKVersion = v
-	}
-
-	if viper.IsSet(configKeyRegistries) {
-		err := viper.UnmarshalKey(configKeyRegistries, &result.RegistryConfigs)
-		if err != nil {
-			return nil, err
-		}
-		if result.RegistryConfigs == nil {
-			// Viper seems to just ignore empty lists.
-			result.RegistryConfigs = tpkg.RegistryConfigs{}
-		}
-	} else if noDefaultRegistry {
-		result.RegistryConfigs = tpkg.RegistryConfigs{}
-	}
-
-	if noAutosync {
-		sync := false
-		result.Autosync = &sync
-	} else if viper.IsSet(configKeyAutosync) {
-		sync := viper.GetBool(configKeyAutosync)
-		result.Autosync = &sync
-	}
-
-	return &result, nil
-}
-
-func (vc *viperConfigStore) Store(ctx context.Context, cfg *commands.Config) error {
-	if cfg.Autosync != nil {
-		viper.Set(configKeyAutosync, *cfg.Autosync)
-	}
-	if cfg.RegistryConfigs != nil {
-		viper.Set(configKeyRegistries, cfg.RegistryConfigs)
-	}
-	return viper.WriteConfig()
 }
