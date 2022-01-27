7d292d2a1ed5d8e8bfd8900a6dff8b467ff74c18
Florian Loitsch <florian@loitsch.com>
2022-01-27 19:23:30 +0100

    Provide a better error for registry downloads.
    We are trying to clone three different branches, 'main', 'master' and
    'trunk'. Generally two of these three give the error
    'couldn't find remote ref "..."'.
    
    If we get a different error, prefer that one.

--- pkg/tpkg/registry.go
+++ pkg/tpkg/registry.go
@@ -344,14 +344,19 @@ func (gr *gitRegistry) Load(ctx context.Context, sync bool, cache Cache, ui UI) 
 
 			var err error
 			for _, branch := range []string{"main", "master", "trunk"} {
-				_, err = git.Clone(ctx, p, git.CloneOptions{
+				_, branchErr := git.Clone(ctx, p, git.CloneOptions{
 					URL:          url,
 					SingleBranch: true,
 					Branch:       branch,
 				})
-				if err == nil {
+				if branchErr == nil {
+					err = nil
 					break
 				}
+				if err == nil || !strings.Contains(branchErr.Error(), "couldn't find remote ref") {
+					err = branchErr
+				}
+
 			}
 			if err != nil {
 				return err
